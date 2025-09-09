7cb0014a60d772e9cdb0d858f5b5e3780b69964e
whitnie7683 <whitnie7683@gmail.com>
2025-09-09 10:18:40 -0700

    pratice

--- unity solo Richards/Assets/NewPlayableAsset.cs
+++ unity solo Richards/Assets/NewPlayableAsset.cs
@@ -7,7 +7,8 @@ using UnityEngine.Playables;
 public class NewPlayableAsset : PlayableAsset
 {
     // Factory method that generates a playable based on this asset
-    public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
+    public override Playable 
+        CreatePlayable(PlayableGraph graph, GameObject go)
     {
         return Playable.Create(graph);
     }
